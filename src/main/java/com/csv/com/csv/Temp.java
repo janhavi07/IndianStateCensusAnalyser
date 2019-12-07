4d020abbcb50ce65955ec4cf60ee22cf086b0757
janhavi07 <partejanhavi@gmail.com>
2019-12-07 13:22:27 +0530

    Refactored:Removed all the commented test cases

---
+++ src/main/java/com/csv/com/csv/Temp.java
@@ -0,0 +1,87 @@
+package com.csv.com.csv;
+
+import com.google.gson.Gson;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class Temp {
+
+    public CsvToBean<CSVCensus> createBuilder() throws CensusExceptions {
+        Reader reader = null;
+        try {
+            reader = Files.newBufferedReader(Paths.get("/home/admin293/Desktop/IndianStateCensus/StateCensusData.csv"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new CensusExceptions(CensusExceptions.ExceptionType.IO_EXCEPTION, "Input file errow");
+        }
+        CsvToBean<CSVCensus> csvToBean = new CsvToBeanBuilder<CSVCensus>(reader)
+                .withType(CSVCensus.class)
+                .withIgnoreLeadingWhiteSpace(true)
+                .build();
+        return csvToBean;
+
+    }
+
+    public int readRecords(String file, Object object) throws CensusExceptions {
+
+        int noOfRecordCount = 0;
+        try {
+            CsvToBean<CSVCensus> csvToBean = createBuilder();
+            Iterator<CSVCensus> csvIterator = csvToBean.iterator();
+            ArrayList<CSVCensus> censusList = new ArrayList<>();
+            while (csvIterator.hasNext()) {
+                CSVCensus census = csvIterator.next();
+                censusList.add(census);
+                noOfRecordCount++;
+            }
+            toSortAccordingToState(censusList);
+            boolean write=writeToGson(censusList);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            throw new CensusExceptions(CensusExceptions.ExceptionType.INCORRECT_DELIMITER, "Incorrect header or delimiter format");
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            throw new CensusExceptions(CensusExceptions.ExceptionType.INCORRECT_TYPE, "Incorrect Type");
+        } catch (CensusExceptions censusExceptions) {
+            censusExceptions.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new CensusExceptions(CensusExceptions.ExceptionType.IO_EXCEPTION,"Wrong file input");
+        }
+        return noOfRecordCount;
+    }
+
+    private boolean writeToGson(ArrayList<CSVCensus> censusList) throws IOException {
+        String SAMPLE_JSON_FILE_PATH="/home/admin293/Desktop/IndianStateCensus/StateCensusDataJson.json";
+        Gson gson=new Gson();
+        String json=gson.toJson(censusList);
+        FileWriter writer=new FileWriter(SAMPLE_JSON_FILE_PATH);
+        writer.write(json);
+        writer.close();
+        return true;
+    }
+
+    private void toSortAccordingToState(ArrayList<CSVCensus> censusList) {
+        Comparator<CSVCensus> c = (s1, s2) -> s1.getState().compareTo(s2.getState());
+        censusList.sort(c);
+    }
+    public static void main(String[] args) {
+        StateCensusAnalyser analyser=new StateCensusAnalyser();
+    }
+    public void sortTheList(ArrayList<CSVCensus> censusList){
+
+            }
+
+
+        }
+    }
+}
--- src/test/java/com/csv/com/csv/StateCensusAnalyserTest.java
+++ src/test/java/com/csv/com/csv/StateCensusAnalyserTest.java
@@ -10,59 +10,59 @@ public class StateCensusAnalyserTest {
    private static final String CLASS_PATH="/home/admin293/Desktop/IndianStateCensus/StateCensusData.csv";
    private static final String CLASS_NAME="com.csv.com.csv.CSVCensus";
 
-//    @Test
-//    public void givenNumberOf_Records_Match_In_CSVStatesData()  {
-//        try {
-//            int count = analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCode.csv",new CSVstates());
-//            Assert.assertEquals(37, count);
-//        } catch (CensusExceptions censusExceptions) {
-//            censusExceptions.printStackTrace();
-//        }
-//    }
+    @Test
+    public void givenNumberOf_Records_Match_In_CSVStatesData()  {
+        try {
+            int count = analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCode.csv",new CSVstates());
+            Assert.assertEquals(37, count);
+        } catch (CensusExceptions censusExceptions) {
+            censusExceptions.printStackTrace();
+        }
+    }
 
 
-//    @Test
-//    public void throws_IOException_In_CSVStatesData() {
-//        try {
-//            analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCod.csv",new CSVstates());
-//        } catch (CensusExceptions censusExceptions) {
-//            censusExceptions.printStackTrace();
-//            Assert.assertEquals(CensusExceptions.ExceptionType.IO_EXCEPTION, censusExceptions.type);
-//        }
-//    }
-//
-//    @Test
-//    public void whenIncorrectFileTye_Throws_Exception_In_CSVStatesData() {
-//        try {
-//
-//            analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCode.pdf",new CSVstates());
-//        } catch (CensusExceptions censusExceptions) {
-//            censusExceptions.printStackTrace();
-//            Assert.assertEquals(CensusExceptions.ExceptionType.INCORRECT_TYPE, censusExceptions.type);
-//
-//        }
-//    }
-//
-//    @Test
-//    public void whenIncorrectDelimiter_Throws_Exception_In_CSVStatesData() {
-//        try {
-//            analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCode.csv",new CSVstates());
-//        } catch (CensusExceptions censusExceptions) {
-//            censusExceptions.printStackTrace();
-//            Assert.assertEquals(CensusExceptions.ExceptionType.INCORRECT_TYPE, censusExceptions.type);
-//        }
-//    }
-//
-//    @Test
-//    public void whenNoHeader_IsGiven_ThrowsException_In_CSVStatesData() {
-//        try {
-//            analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCode.csv", new CSVstates());
-//        } catch (CensusExceptions censusExceptions) {
-//            censusExceptions.printStackTrace();
-//            //Assert.assertEquals(CensusExceptions.ExceptionType.INCORRECT_TYPE, censusExceptions.type);
-//
-//        }
-//    }
+    @Test
+    public void throws_IOException_In_CSVStatesData() {
+        try {
+            analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCod.csv",new CSVstates());
+        } catch (CensusExceptions censusExceptions) {
+            censusExceptions.printStackTrace();
+            Assert.assertEquals(CensusExceptions.ExceptionType.IO_EXCEPTION, censusExceptions.type);
+        }
+    }
+
+    @Test
+    public void whenIncorrectFileTye_Throws_Exception_In_CSVStatesData() {
+        try {
+
+            analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCode.pdf",new CSVstates());
+        } catch (CensusExceptions censusExceptions) {
+            censusExceptions.printStackTrace();
+            Assert.assertEquals(CensusExceptions.ExceptionType.INCORRECT_TYPE, censusExceptions.type);
+
+        }
+    }
+
+    @Test
+    public void whenIncorrectDelimiter_Throws_Exception_In_CSVStatesData() {
+        try {
+            analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCode.csv",new CSVstates());
+        } catch (CensusExceptions censusExceptions) {
+            censusExceptions.printStackTrace();
+            Assert.assertEquals(CensusExceptions.ExceptionType.INCORRECT_TYPE, censusExceptions.type);
+        }
+    }
+
+    @Test
+    public void whenNoHeader_IsGiven_ThrowsException_In_CSVStatesData() {
+        try {
+            analyserTest.readRecords("/home/admin293/Desktop/IndianStateCensus/StateCode.csv", new CSVstates());
+        } catch (CensusExceptions censusExceptions) {
+            censusExceptions.printStackTrace();
+            //Assert.assertEquals(CensusExceptions.ExceptionType.INCORRECT_TYPE, censusExceptions.type);
+
+        }
+    }
 
     @Test
     public void givenNumberOf_RecordsShould_Match()  {
